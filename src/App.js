26759648ec7d0ee917adb6e640d6894341c40c4a
nerdfiles <nerdfiles@gmail.com>
2022-09-22 11:34:32 -0500

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,92 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss'
+import { useState } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+/**
+ * parser.
+ *
+ * @param {} input
+ * @param {} callback
+ */
+const parser = (input, callback) => {
+  const VALUE = input
+  callback(VALUE)
 }
 
-export default App;
+/**
+ * Input.
+ *
+ * @param {} props
+ */
+const Input = (props) => {
+
+  return (
+    <label htmlFor=''>
+      <input
+        id='chord-input'
+        name='chord-input'
+        required
+        aria-required='true'
+        placeholder={props?.name}
+      />
+    </label>
+  )
+}
+
+/**
+ * Setting.
+ *
+ * @param {} props
+ */
+const Setting = (props) => {
+  const GLUE = ''
+
+  return (
+    <button
+      title={[
+        'setting: ',
+        props?.name
+      ].join(GLUE)}
+      onClick={(e) => parser(props?.name, props?.setMode)}
+    >
+      {props?.name}
+    </button>
+  )
+}
+
+/**
+ * Chordlike.
+ *
+ * @param {} props
+ */
+const Chordlike = (props) => {
+  const [mode, setMode] = useState('math')
+  // @note that we're configuring the input box for mobile, assistive tools.
+  const DEFAULT_MODE = 'math' // '([0-9]*)', '+', '-', ...
+  const LOGIC_MODE = 'fol' // '!', '>', '&', 'v', '()', '\∀', ...
+  const TELEPHONE_MODE = 'tel' // '#', '*', ([0-9])
+  const SEARCH_MODE = 'searchbox' // '([A-Za-z])', suggest logical operations and advanced search recipes as typing goes
+  const MODE_SETTING = mode || DEFAULT_MODE
+
+  return (
+    <>
+      <Input name={MODE_SETTING} />
+      <Setting name={DEFAULT_MODE} setMode={setMode} />
+      <Setting name={LOGIC_MODE} setMode={setMode} />
+      <Setting name={TELEPHONE_MODE} setMode={setMode} />
+      <Setting name={SEARCH_MODE} setMode={setMode} />
+    </>
+  )
+}
+
+/**
+ * App.
+ */
+function App () {
+  return (
+    <>
+      <Chordlike />
+    </>
+  )
+}
+
+export default App
