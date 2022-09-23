cd1af94f3b30fc4f67f8d2bd3dce696a4db42e43
nerdfiles <nerdfiles@gmail.com>
2022-09-23 12:19:05 -0500

    this is all wrong. all this code simply goes to the backend to receive whatever input value is entered from the keyboard2form

--- src/Input/index.js
+++ src/Input/index.js
@@ -33,9 +33,13 @@ const Input = (props) => {
 
   /**
    * @name onChangeHandler
+   * @description change handler.
+   * @todo should probably use the dispatching method or something other than 
+   * this! sure it's a handler, but it's too many lines!
    * @param {Event} event - .
    */
   const onChangeHandler = (event) => {
+    const DEFAULT_OP = '+'
     const VALUE = event && event.target && event.target.value
     const PARTS = VALUE.split(UNGLUE)
     const DATA = PARTS[PARTS.length - 1]
@@ -45,23 +49,33 @@ const Input = (props) => {
     // grouping input numbers until there's a NaN. it ain't smart and it ain't 
     // fast but it also ain't cheap.
     if (PARTS.length && isNaN(DATA_INPUT)) {
-      // const operator = PARTS && PARTS.splice(PARTS.length-1, 1)
+      const OP_CODE = PARTS && PARTS.splice(PARTS.length-1, 1)
+      log('💧 storing OP_CODE:', OP_CODE)
       const outcome = PARTS.reduce((accumulatorRef, currentRef) => {
         return accumulatorRef + currentRef
       })
 
-      STORE.push(outcome)
+      STORE.push([OP_CODE, outcome])
+      //STORE.push(outcome)
     }
 
     // @todo gettin' hairy
-    const EXPRESSION = STORE[0]
-    const OP_CHAR = '+'
+    const OPERATOR_POSITION = 1
+    const EXPRESSION = STORE && STORE.length && STORE[0][1]
+    log(EXPRESSION)
+    const EX = EXPRESSION && EXPRESSION.split(UNGLUE)
+    log(EX)
+    const OP_CHAR = EX[OPERATOR_POSITION] || DEFAULT_OP
+    log(OP_CHAR)
     const OPERANDS = EXPRESSION && EXPRESSION.split(OP_CHAR)
+    log(OPERANDS)
 
+    // will do nothing if computer can't split by the input in question
     if (OPERANDS && OPERANDS.length > 1) {
       const leftOperand = Number(OPERANDS[0])
       const rightOperand = Number(OPERANDS[1])
       const OUTPUT = leftOperand + rightOperand
+      log(OUTPUT)
 
       SETINPUT(OUTPUT)
     }
